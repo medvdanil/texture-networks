ffec9b9a4d7dc2c2d3607fc001b6abaffb57303f
George Hoqqanen <george@hoqqanen.com>
2016-04-17 13:34:34 -0700

    Add perceptual loss network, data batcher
    Also cleaned up the Ulyanov network so that network initialization and training are separated, as well as the VGG network so that it can take in a stack of style, content, and synthesized images for faster processing and lower memory usage.

---
+++ perceptual_loss_network.py
@@ -0,0 +1,86 @@
+"""
+Implementation of style transfer network defined in "Perceptual Losses for Real-Time
+Style Transfer and Super-Resolution", http://arxiv.org/abs/1603.08155
+"""
+
+import tensorflow as tf
+
+from coco_data import COCODataBatcher
+from network_helpers import conv2d_with_weights, spatial_batch_norm, load_image
+from style_helpers import total_variation
+from vgg_network import VGGNetwork
+
+
+def nonresidual_block(input_layer, num_filters):
+    """
+    Constructs a block without residual
+    """
+    with tf.get_default_graph().name_scope('nonresidual-block'):
+        conv1 = conv2d_with_weights(input_layer, 3, num_filters)
+        bn1 = spatial_batch_norm(conv1)
+        relu = tf.nn.relu(bn1)
+        conv2 = conv2d_with_weights(relu, 3, num_filters)
+        output = spatial_batch_norm(conv2)
+    return output
+
+
+def residual_block(input_layer, num_filters):
+    """
+    Constructs a block as detailed in http://cs.stanford.edu/people/jcjohns/papers/fast-style/fast-style-supp.pdf
+    """
+    with tf.get_default_graph().name_scope('residual-block'):
+        output = tf.add(nonresidual_block(input_layer, num_filters), input_layer)
+    return output
+
+
+class PerceptualLossNetwork(object):
+    inputDimension = 224
+    batchSize = 4
+
+    def __init__(self, style_image_path):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            inputDim = self.inputDimension
+            batchSize = self.batchSize
+            halfInputDim = int(inputDim / 2)
+            quarterInputDim = int(inputDim / 4)
+            self.content_image = tf.placeholder(tf.float32, [batchSize, inputDim, inputDim, 3])
+            layer1 = conv2d_with_weights(self.content_image, 9, 32)
+            layer2 = conv2d_with_weights(layer1, 3, 64, stride=2)
+            layer3 = conv2d_with_weights(layer2, 3, 128, stride=2)
+            layer4 = residual_block(layer3, 128)
+            layer5 = residual_block(layer4, 128)
+            layer6 = residual_block(layer5, 128)
+            layer7 = residual_block(layer6, 128)
+            layer8 = residual_block(layer7, 128)
+            layer9 = tf.nn.conv2d_transpose(layer8, tf.Variable(tf.random_uniform([quarterInputDim, quarterInputDim, 64, 128])), [batchSize, halfInputDim, halfInputDim, 64], [1, 2, 2, 1])
+            layer10 = tf.nn.conv2d_transpose(layer9, tf.Variable(tf.random_uniform([halfInputDim, halfInputDim, 32, 64])), [batchSize, inputDim, inputDim, 32], [1, 2, 2, 1])
+            self.output = conv2d_with_weights(layer10, 3, 3)
+            self.style_image = tf.to_float(tf.reshape(tf.constant(load_image(style_image_path)), [1, 224, 224, 3]))
+
+            vgg_input = tf.concat(0, [self.style_image, self.content_image, self.output])
+            # Feed output to VGG, compute loss against input image and style image.
+            vgg_net = VGGNetwork('vgg', vgg_input, 1, self.batchSize, self.batchSize)
+            self.style_loss = vgg_net.style_loss([(1, 2), (2, 2), (3, 3), (4, 3)])
+            self.content_loss = vgg_net.content_loss([(2, 2)])
+            # Alternatively, self.network_loss = vgg_net.combined_loss([(1, 2), (2, 2), (3, 3), (4, 3)], [(2, 2)])
+            # but this is currently more useful for debugging and inspecting the losses separately
+            self.network_loss = tf.add(self.style_loss, self.content_loss)
+            self.variation_regularizer_loss = total_variation(self.output)  # For smoothing
+            self.loss = tf.add(self.network_loss, self.variation_regularizer_loss)
+
+    def run_train(self, epochs):
+        with self.graph.as_default():
+            # Gradient descent. Draw minibatches of images from COCO.
+            batcher = COCODataBatcher('data')
+            optimizer = tf.train.AdamOptimizer(learning_rate=0.1, beta1=0.9, beta2=0.999, epsilon=1e-08,
+                                               use_locking=False, name='Adam')
+            train_step = optimizer.minimize(self.loss)
+
+            with tf.Session() as sess:
+                init = tf.initialize_all_variables()
+                sess.run(init)
+                for e in range(epochs):
+                    batch = batcher.get_batch(self.batchSize)
+                    train_step.run(feed_dict={self.content_image: batch})
+                    print("loss after ", e, sess.run(self.loss, feed_dict={self.content_image: batch}))
--- style_helpers.py
+++ style_helpers.py
@@ -13,3 +13,27 @@ def gramian(activations):
     transposed_vectorized_activations = tf.transpose(vectorized_activations, perm=[0, 2, 1])
     mult = tf.batch_matmul(vectorized_activations, transposed_vectorized_activations)
     return mult
+
+
+def total_variation(image_batch):
+    """
+    :param image_batch: A 4D tensor of shape [batch_size, width, height, channels]
+    """
+    batch_shape = image_batch.get_shape().as_list()
+    width = batch_shape[1]
+    left = tf.slice(image_batch, [0, 0, 0, 0], [-1, width - 1, -1, -1])
+    right = tf.slice(image_batch, [0, 1, 0, 0], [-1, -1, -1, -1])
+
+    height = batch_shape[2]
+    top = tf.slice(image_batch, [0, 0, 0, 0], [-1, -1, height - 1, -1])
+    bottom = tf.slice(image_batch, [0, 0, 1, 0], [-1, -1, -1, -1])
+
+    # left and right are 1 less wide than the original, top and bottom 1 less tall
+    # In order to combine them, we take 1 off the height of left-right, and 1 off width of top-bottom
+    horizontal_diff = tf.slice(tf.sub(left, right), [0, 0, 0, 0], [-1, -1, height - 1, -1])
+    vertical_diff = tf.slice(tf.sub(top, bottom), [0, 0, 0, 0], [-1, width - 1, -1, -1])
+
+    sum_of_pixel_diffs_squared = tf.add(tf.square(horizontal_diff), tf.square(vertical_diff))
+    total_variation = tf.reduce_sum(tf.sqrt(sum_of_pixel_diffs_squared))
+    # TODO: Should this be normalized by the number of pixels?
+    return total_variation
